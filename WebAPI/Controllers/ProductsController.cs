4cc8040fc29ed9b36c637ebbf467ae2fff373d16
Adem Furkan Kavuş <150111027@ogrenci.yalova.edu.tr>
2021-02-13 23:56:59 +0300

    13 Şubat Ders Sonu

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //Attribute javada annotation:bir class ile ilgili bilgi verme.
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupling gevşek bağlılık soyuta bağlılık var.
+        //naming convetntion isimlendirme standardı.
+        
+        //Injection
+        //IoC Container -- Inversion of Control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        //Injection
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            //Swagger
+            //Dependancy chain --
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+
+            }
+            return BadRequest(result);
+
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+    }
+}
